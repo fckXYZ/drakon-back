464f646d7b783c52a355ceaa1810a89272bacc81
Artem Kovtun <rtmkvtn@gmail.com>
2021-09-19 10:20:02 +0800

    add Dockerfile

---
+++ .adminbro/.entry.js
@@ -0,0 +1,41 @@
+AdminBro.UserComponents = {}
+import Component1 from '../server/models/components/upload-file.edit'
+AdminBro.UserComponents.Component1 = Component1
+import Component2 from '../server/models/components/upload-file.list'
+AdminBro.UserComponents.Component2 = Component2
+import Component3 from '../server/models/components/upload-file.edit'
+AdminBro.UserComponents.Component3 = Component3
+import Component4 from '../server/models/components/upload-file.list'
+AdminBro.UserComponents.Component4 = Component4
+import Component5 from '../node_modules/@admin-bro/upload/src/features/upload-file/components/edit'
+AdminBro.UserComponents.Component5 = Component5
+import Component6 from '../node_modules/@admin-bro/upload/src/features/upload-file/components/list'
+AdminBro.UserComponents.Component6 = Component6
+import Component7 from '../node_modules/@admin-bro/upload/src/features/upload-file/components/show'
+AdminBro.UserComponents.Component7 = Component7
+import Component8 from '../server/models/components/text-reqs'
+AdminBro.UserComponents.Component8 = Component8
+import Component9 from '../server/models/components/text-reqs'
+AdminBro.UserComponents.Component9 = Component9
+import Component10 from '../node_modules/@admin-bro/upload/src/features/upload-file/components/edit'
+AdminBro.UserComponents.Component10 = Component10
+import Component11 from '../node_modules/@admin-bro/upload/src/features/upload-file/components/list'
+AdminBro.UserComponents.Component11 = Component11
+import Component12 from '../node_modules/@admin-bro/upload/src/features/upload-file/components/show'
+AdminBro.UserComponents.Component12 = Component12
+import Component13 from '../server/models/components/album-tracks.list'
+AdminBro.UserComponents.Component13 = Component13
+import Component14 from '../server/models/components/album-tracks.list'
+AdminBro.UserComponents.Component14 = Component14
+import Component15 from '../node_modules/@admin-bro/upload/src/features/upload-file/components/edit'
+AdminBro.UserComponents.Component15 = Component15
+import Component16 from '../node_modules/@admin-bro/upload/src/features/upload-file/components/list'
+AdminBro.UserComponents.Component16 = Component16
+import Component17 from '../node_modules/@admin-bro/upload/src/features/upload-file/components/show'
+AdminBro.UserComponents.Component17 = Component17
+import Component18 from '../node_modules/@admin-bro/upload/src/features/upload-file/components/edit'
+AdminBro.UserComponents.Component18 = Component18
+import Component19 from '../node_modules/@admin-bro/upload/src/features/upload-file/components/list'
+AdminBro.UserComponents.Component19 = Component19
+import Component20 from '../node_modules/@admin-bro/upload/src/features/upload-file/components/show'
+AdminBro.UserComponents.Component20 = Component20
